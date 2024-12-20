0568ad252f86782dc285b4cddd3394c8435a3eb3
Alok Das <65537690+Razer256g4@users.noreply.github.com>
2024-12-20 11:25:52 +1100

    Add files via upload

---
+++ coin.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+
+
+
+@onready var game_manager: Node = %"Game Manager"
+
+
+func _on_body_entered(body: Node2D) -> void:
+	print("+1 Coin")
+	game_manager.add_point()
+	queue_free()
---
+++ game_manager.gd
@@ -0,0 +1,9 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+var score:int = 0
+
+func add_point():
+	score=score+1
+	print(score)
